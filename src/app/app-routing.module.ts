0a01c696dce058cea427145cf3590af13c885738
fbonifac94 <facu.bonifacich@hotmail.com>
2020-11-09 18:35:52 -0300

    Corrección rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,36 +7,36 @@ import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { ProveedoresComponent } from './proveedores/proveedores.component';
 import { ProductosComponent } from './productos/productos.component';
-import { FooterComponent } from './footer/footer.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
-  { 
-      path: 'principal', 
-      component: PrincipalComponent,
-      canActivate: [AuthGuard],
-      children: [
-         { path: 'categorias', component: CategoriasComponent},
-         { path: 'proveedores/:idProveedor', component: ProveedoresComponent },
-         { path: 'productos', component: ProductosComponent },
-         { path: 'footer', component: FooterComponent},
-         { path: 'contacto', component: ContactoComponent},
-         /* { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica #1' }},
-          { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil' }},
-          { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' }},
-          { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
-          { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }},
-          { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busqueda general' }},
+  {
+    path: '',
+    component: PrincipalComponent,
+    children: [
+      { path: '', component: InicioComponent },
+      { path: 'categorias', canActivate: [AuthGuard], component: CategoriasComponent },
+      { path: 'proveedores/:idProveedor', canActivate: [AuthGuard], component: ProveedoresComponent },
+      { path: 'productos', canActivate: [AuthGuard], component: ProductosComponent },
+      { path: 'contacto', component: ContactoComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+      /* { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica #1' }},
+       { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil' }},
+       { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' }},
+       { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
+       { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }},
+       { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busqueda general' }},
 
-          // Mantenimientos
-          { path: 'usuarios', canActivate: [AdminGuard] , component: UsuariosComponent, data: { titulo: 'Usuarios' }},
-          { path: 'medicos', component: MedicosComponent, data: { titulo: 'Médicos' }},
-          { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Detalle del médico' }},
-          { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Hospitales' }},*/
-      ]
+       // Mantenimientos
+       { path: 'usuarios', canActivate: [AdminGuard] , component: UsuariosComponent, data: { titulo: 'Usuarios' }},
+       { path: 'medicos', component: MedicosComponent, data: { titulo: 'Médicos' }},
+       { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Detalle del médico' }},
+       { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Hospitales' }},*/
+    ]
   },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+
 ];
 
 @NgModule({
