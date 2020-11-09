010316c890503d188bec014352f4704ec0e05f86
alexandervaca.alexvaca <alexandervaca.alex_andy_25@yahoo.com.ar>
2020-11-09 12:31:50 -0300

    se elimina categories ya en desuso. corrijo un import en admin.guard.ts

--- src/shared/guards/admin.guard.ts
+++ src/shared/guards/admin.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import {UsuarioService } from '../services/usuario.service';
+import {UsuariosService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  constructor(private _usuarioService: UsuarioService, private router: Router) {}
+  constructor(private _usuarioService: UsuariosService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      if (this._usuarioService.role !== 'ADMIN_ROLE') {
+      if (this._usuarioService.rol !== 'ADMIN_ROLE') {
         this.router.navigateByUrl('/dashboard');
         return false;
       }
